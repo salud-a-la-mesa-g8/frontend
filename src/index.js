5455de5e9602aecd9c26cfe8cda9e3b0949ed94e
Alexander Pachón <alexander.pachon@gmail.com>
2021-11-08 20:20:34 -0500

    cambios en el index

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 // componenentes
-// import MenuSuperior from "./components/MenuSuperior";
-// import PiePagina from "./components/PiePagina";
-// import InicioSitio from "./components/InicioSitio";
-import CounterApp from "./components/CounterApp";
+import MenuSuperior from "./components/MenuSuperior";
+import PiePagina from "./components/PiePagina";
+import InicioSitio from "./components/InicioSitio";
+// import CounterApp from "./components/CounterApp";
 
 ReactDOM.render(
 	<React.StrictMode>
-		{/* <MenuSuperior />		
+		<MenuSuperior />		
     <InicioSitio />
-		<PiePagina /> */}
-    <CounterApp value="{10}" subtitle = "el subtítulo " />
+		<PiePagina />
+    {/* <CounterApp value="{10}" subtitle = "el subtítulo " /> */}
 	</React.StrictMode>,
 	document.getElementById("root")
 );
